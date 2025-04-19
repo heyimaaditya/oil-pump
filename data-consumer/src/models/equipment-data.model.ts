985ae16ec0226b40eeac844df174c88992d50aad
heyimaaditya <aadityatyagi158@gmail.com>
2025-04-19 19:01:28 +0530

    added:data-consumer model

---
+++ data-consumer/src/models/equipment-data.model.ts
@@ -0,0 +1,12 @@
+export interface EquipmentDataModel {
+    timestamp: string;
+    suction_pressure: number;
+    discharge_pressure: number;
+    flow_rate: number;
+    fluid_temperature: number;
+    bearing_temperature: number;
+    vibration: number;
+    impeller_speed: number;
+    lubrication_oil_level: number;
+    npsh: number;
+}
